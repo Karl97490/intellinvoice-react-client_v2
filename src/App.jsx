46ad35fb2ee5358aef21f650a1fc5abf5bdd5d92
Karl97490 <karl.pery@icloud.com>
2026-07-07 13:04:07 +0400

    first init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+      <h1>This is App component</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
