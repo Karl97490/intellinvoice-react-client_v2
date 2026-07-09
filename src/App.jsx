82b65868c6437e8504ada9b1cf6f4ec3f55e68cc
Karl97490 <karl.pery@icloud.com>
2026-07-09 20:32:53 +0400

    setup axios service index file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import { Button } from "flowbite-react";
+import service from "./services/index.service";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    testApi();
+  });
+
+  const testApi = async () => {
+    try {
+      const response = await service.test();
+      console.log(response);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   return (
     <>
       <h1>This is App component</h1>
---
+++ src/services/index.service.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+class Service {
+  constructor() {
+    this.service = axios.create({
+      baseURL: import.meta.env.VITE_API_UR || "http://localhost:5005",
+    });
+    this.service.interceptors.request.use((config) => {
+      const storedToken = localStorage.getItem("authToken");
+      if (storedToken) {
+        config.headers = { authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+
+  test = () => {
+    return this.service.get("/");
+  };
+}
+
+const service = new Service();
+
+export default service;
