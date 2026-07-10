899c52b961d8cc0b29e4360ed1a7fd44edf1575d
Karl97490 <karl.pery@icloud.com>
2026-07-10 09:09:12 +0400

    setup signup submit method with try catch

--- src/pages/public/Signup.jsx
+++ src/pages/public/Signup.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Button, Label, TextInput, Spinner } from "flowbite-react";
 import { Link } from "react-router-dom";
+import authService from "../../services/auth.service";
 
 const Signup = () => {
   const [signupForm, setSignupForm] = useState({
@@ -10,11 +11,32 @@ const Signup = () => {
     email: "",
     password: "",
   });
-  const [isSignup, setIsSignup] = useState(false);
+  const [isSignuping, setIsSignuping] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {}, []);
-  const handleSignup = async (e) => {};
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    console.log("signup...");
+    setIsSignuping(true);
+    const body = {
+      ...signupForm,
+    };
+    try {
+      // const response = await authService.signup(body);
+      // console.log(response);
+      console.log(body);
+      await new Promise((_, reject) =>
+        setTimeout(() => reject("Promise rejected."), 1000),
+      );
+      setIsSignuping(false);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      setIsSignuping(false);
+      setErrorMessage(error);
+    }
+  };
   const handleChange = (e) => {
     const { name, value } = e.target;
     setSignupForm((prev) => ({
@@ -25,7 +47,7 @@ const Signup = () => {
   return (
     <>
       <h1>Signup component</h1>
-      <form className="flex max-w-md flex-col gap-4">
+      <form className="flex max-w-md flex-col gap-4" onSubmit={handleSignup}>
         <div className="flex gap-2">
           <div className="mb-2 block w-full">
             <Label htmlFor="firstName">First Name</Label>
@@ -81,9 +103,18 @@ const Signup = () => {
           />
         </div>
         <div className="flex justify-center">
-          <p className="text-red-400">Error Message</p>
+          {errorMessage && <p className="text-red-400">{errorMessage}</p>}
         </div>
-        <Button type="submit">Register new account</Button>
+        <Button type="submit" className="cursor-pointer">
+          {isSignuping ? (
+            <>
+              <Spinner aria-label="Spinner loading button" size="sm" />
+              <span className="pl-3">Loading...</span>
+            </>
+          ) : (
+            <>Register new account</>
+          )}
+        </Button>
         <div className="flex justify-center gap-1">
           <p>Already have an account?</p>
           <Link to="/login" className="text-blue-500 font-medium">
