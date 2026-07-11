dbb5964e234a7d85f1a219ad8f2c93b2464da978
Karl97490 <karl.pery@icloud.com>
2026-07-11 21:42:55 +0400

    set get all clients request

--- src/pages/private/Clients/AllClients.jsx
+++ src/pages/private/Clients/AllClients.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import clientService from "../../../services/client.service";
+import { useNavigate } from "react-router-dom";
+
 const AllClients = () => {
+  const [clients, setClients] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await clientService.allClients();
+      console.log(response);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.response);
+      // navigate("/error-page"); // Redirect to error-page
+    }
+  };
   return <h1>AllClients component...</h1>;
 };
 
