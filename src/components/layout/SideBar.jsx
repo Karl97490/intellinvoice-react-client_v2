9d485103de4926275366e85be8ef2b903b4220c5
Karl97490 <karl.pery@icloud.com>
2026-07-16 16:59:28 +0400

    start styling SideBar component

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,5 +1,51 @@
+import {
+  Sidebar,
+  SidebarCollapse,
+  SidebarItem,
+  SidebarItemGroup,
+  SidebarItems,
+} from "flowbite-react";
+import { ChartPie } from "lucide-react";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth.context";
+
 const SideBar = () => {
-  return <h1>SideBar component</h1>;
+  const { isLoggedIn } = useContext(AuthContext);
+
+  if (isLoggedIn) {
+    return (
+      <Sidebar className="h-screen">
+        <SidebarItems>
+          <SidebarItemGroup>
+            <SidebarItem href="#" icon={undefined}>
+              Dashboard
+            </SidebarItem>
+            <SidebarCollapse icon={undefined} label="E-commerce">
+              <SidebarItem href="#">Products</SidebarItem>
+              <SidebarItem href="#">Sales</SidebarItem>
+              <SidebarItem href="#">Refunds</SidebarItem>
+              <SidebarItem href="#">Shipping</SidebarItem>
+            </SidebarCollapse>
+            <SidebarItem href="#" icon={undefined}>
+              Inbox
+            </SidebarItem>
+            <SidebarItem href="#" icon={undefined}>
+              Users
+            </SidebarItem>
+            <SidebarItem href="#" icon={undefined}>
+              Products
+            </SidebarItem>
+            <SidebarItem href="#" icon={undefined}>
+              Sign In
+            </SidebarItem>
+            <SidebarItem href="#" icon={undefined}>
+              Sign Up
+            </SidebarItem>
+          </SidebarItemGroup>
+        </SidebarItems>
+      </Sidebar>
+    );
+  }
 };
 
 export default SideBar;
